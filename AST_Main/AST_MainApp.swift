e43557e2f54cb8f7411e51d07e744358bc534e6e
Samuel Valasek <hello@samvalasek.me>
2023-11-23 15:17:31 +0000

    Initial Commit

---
+++ AST_Main/AST_MainApp.swift
@@ -0,0 +1,17 @@
+//
+//  AST_MainApp.swift
+//  AST_Main
+//
+//  Created by Sam Valasek on 23/11/2023.
+//
+
+import SwiftUI
+
+@main
+struct AST_MainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
